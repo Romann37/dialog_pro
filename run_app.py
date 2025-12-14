69bff7b53288e3c82a4e98e0034a63daf3abc40d
Romann37 <mihalkovo62@gmail.com>
2025-12-14 11:59:16 +0300

    Fix Render2

--- run_app.py
+++ run_app.py
@@ -1,7 +1,20 @@
 import subprocess
 import sys
 
-subprocess.run(
-    [sys.executable, "-m", "streamlit", "run", "dialog_pro.py"],
-    check=True,
-)
+def main():
+    try:
+        result = subprocess.run(
+            [sys.executable, "-m", "streamlit", "run", "dialog_pro.py"],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        print(result.stdout)
+        print(result.stderr)
+    except Exception as e:
+        print("Ошибка при запуске Streamlit:", e)
+        input("Нажмите Enter для выхода...")
+
+if __name__ == "__main__":
+    main()
+
